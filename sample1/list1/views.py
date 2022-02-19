96a8e1bcc08a9df505d1335e644aaf45f8726c6f
afsul <afsulult@gmail.com>
2022-02-19 12:02:33 +0530

    second commit

--- sample1/list1/views.py
+++ sample1/list1/views.py
@@ -4,8 +4,8 @@ from.models import list1
 
 def index(request):
     list_image = list1.objects.all()
-    imgs= list1.objects.get(id=1)
+    
     # return HttpResponse("<h1>Welcome to django</h1>")
-    return render(request, 'index.html',{'ls':list_image,'imgs':imgs})
+    return render(request, 'index.html',{'ls':list_image})
 
 # Create your views here.
